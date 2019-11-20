d4eed64ceb5a6dcfa58fce2ea5c4ec1ef9a0c108
gajaIeva <gaja.ieva@gmail.com>
2019-11-20 15:57:36 +0200

    ProductItem updated (checkboxes from LS, index)

--- talech-app/src/components/ProductItem.js
+++ talech-app/src/components/ProductItem.js
@@ -3,22 +3,26 @@ import React, {Component, Fragment} from 'react';
 
 class ProductItem extends Component {
 
+
     render () {
         
         let products = JSON.parse(localStorage.getItem('products'));
 
         console.log(products);
         
-        let productListElements = products.map((product) => 
+        let productListElements = products.map((product, index) => 
         <Fragment>
             <tr>
-                <td>1</td>
+                <td key={index}>{index+1}</td>
                 <td>{product.product_name}</td>
                 <td>{product.product_EAN}</td>
                 <td>{product.product_type}</td>
                 <td>{product.product_weight}</td>
                 <td>{product.product_color}</td>
-                <td><input type="checkbox" {...product.product_isActive} /></td>
+                <td><input type="checkbox" 
+                           checked={product.product_isActive}
+                           //onChange={this.onChangeProductIsActive}
+                           /></td>
                 <td><button className="btn btn-outline-info">View</button></td>
                 <td><button className="btn btn-outline-warning">Edit</button></td>
                 <td><button className="btn btn-outline-danger">Delete</button></td>
